7446e1485b4f53d1ea9ca81c5f603f1d9718acea
ajaberi <adnan.ajaberi@gmail.com>
2023-10-04 16:01:30 +0300

    fix

--- pkg/sort.go
+++ pkg/sort.go
@@ -6,7 +6,7 @@ import (
 
 // Sorts the files and folders by alphabteical order
 func sortFilesAndFolders(filesAndFolders []File, flags Flags) []File {
-	sort.SliceStable(filesAndFolders, func(i, j int) bool {
+	sort.Slice(filesAndFolders, func(i, j int) bool {
 		if flags.Rr {
 			return filesAndFolders[i].Info.Name() > filesAndFolders[j].Info.Name()
 		}
